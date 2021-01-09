535a480c2cb5829011e3b24034bf908e122a5c04
paddy <paddy@paddy.paddy>
2021-01-09 15:05:45 +0700

    commit

--- bot/bot.js
+++ bot/bot.js
@@ -1,16 +1,8 @@
 
 const TelegramBot = require('node-telegram-bot-api');
-// const Agent = require('socks5-https-client/lib/Agent');
-const token = '1489365929:AAHl6rCZmXgZgfhukKelaf0uYlrfoxcMToo';
+const token = '';
 var bot = new TelegramBot(token, {
     polling: true,
-    // request: {
-    //     agentClass: Agent,
-    //     agentOptions: {
-    //         socksHost: '127.0.0.1',
-    //         socksPort: 10808
-    //     }
-    // }
 });
 
 module.exports = bot;
